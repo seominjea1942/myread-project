8433301c4553d96de3cfc9670cb380b654181457
seominjea1942 <seominjea1942@gmail.com>
2022-01-08 14:08:42 -0800

    Search functions updated

--- src/Book.js
+++ src/Book.js
@@ -6,7 +6,7 @@ function Book({book, changeStatus, bookStatus}) {
         <li>
         <div className="book">
             <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks.thumbnail?`url(${book.imageLinks.thumbnail})`:null}}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks?`url(${book.imageLinks.thumbnail})`:null}}></div>
                 <div className="book-shelf-changer">
                     <select onChange={(e)=>{
 changeStatus(e.target.value, book)
--- src/CreateBook.js
+++ src/CreateBook.js
@@ -11,25 +11,25 @@ function CreateBook({searchBook, searchedBooks,changeStatus, bookStatus, setSear
               onClick={()=>setSearchedBooks([])}
               >Go back</Link>
               <div className="search-books-input-wrapper">
-                <input type="text" onKeyUp={(e)=>{
-                    if (e.keyCode === 13) {
+                <input type="text" onChange={(e)=>{
                     searchBook(e.target.value)
-                    }
                 }} placeholder="Search by title or author"/>
               </div>
             </div>
-            <div className="search-term-example">
-                <h5>Search term example</h5>
-                <p>'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'</p>
-            </div>
             <div className="search-books-results">
               <ol className="books-grid">
-                  {searchedBooks.map(book=><Book
+                  {(searchedBooks&&searchedBooks.length>0)?
+                  searchedBooks.map(book=><Book
                   book={book}
                   key={uuid()}
                   changeStatus={changeStatus}
                   bookStatus={bookStatus}
-                  />)}
+                  />):
+                  <div className="search-term-example">
+                <h5>Try this keyword to Search</h5>
+                <p>'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'</p>
+            </div>
+                  }
               </ol>
             </div>
           </div>
